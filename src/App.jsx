fc6a9ad06bf2f5bf0d6497efe482b640b7b262dc
BoatGT-9 <twz192545@gmail.com>
2023-11-13 22:09:12 +0700

    commit-Bmi

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import BMICalculator from './BMICalculatorComponent'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <BMICalculator/>
+    </>
+  )
+}
+
+export default App
